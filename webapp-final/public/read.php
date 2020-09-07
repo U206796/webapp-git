8c276730d55a25040b23ced98b5a1cf5a8c3f91b
u3206769 <alex.catherine@live.com>
2020-09-07 16:26:55 +1000

    updates_to_read

--- webapp-final/public/read.php
+++ webapp-final/public/read.php
@@ -1,7 +1,61 @@
+<?php 
+if (isset($_POST['submit'])) {
+	
+    require "../config.php"; 
+    
+	try {
+        $connection = new PDO($dsn, $username, $password, $options);
+		
+        $sql = "SELECT * FROM reviews";
+
+        $statement = $connection->prepare($sql);
+        $statement->execute();
+        
+        $result = $statement->fetchAll();
+
+	} catch(PDOException $error) {
+		echo $sql . "<br>" . $error->getMessage();
+	}	
+}
+?>
+
 <?php include "templates/header.php"; ?>
 
+
+<?php  
+    if (isset($_POST['submit'])) {
+        if ($result && $statement->rowCount() > 0) { ?>
+<h2>Results</h2>
+
+<?php 
+                foreach($result as $row) { 
+            ?>
+
+<p>
+    ID:
+    <?php echo $row["id"]; ?><br> Venue Name:
+    <?php echo $row['venuename']; ?><br> Suburb:
+    <?php echo $row['location']; ?><br> Date Attended:
+    <?php echo $row['dateattended']; ?><br> Atmosphere:
+    <?php echo $row['atmosphere']; ?><br> Menu Options:
+    <?php echo $row['menu']; ?><br>
+</p>
+<?php 
+        
+        ?>
+
+<hr>
+<?php };
+        }; 
+    }; 
+?>
+
+
 <form method="post">
-    <input type="submit" name="Submit" value="View all">
+
+    <input type="submit" name="submit" value="View all">
+
 </form>
+
 
 <?php include "templates/footer.php"; ?>
